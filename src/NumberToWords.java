8e9a2512cfc129397d6a8e0b57d8bb1c81b5b418
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2024-07-05 03:21:37 +0300

    New classes

---
+++ src/NumberToWords.java
@@ -0,0 +1,82 @@
+public class NumberToWords {
+
+    public static void numberToWords(int number) {
+        if (number < 0) {
+            System.out.println("Invalid Value");
+        }
+
+        int reverseNumber = reverse(number);
+        int originalDigits = getDigitCount(number);
+        int reversedDigits = getDigitCount(reverseNumber);
+
+        while (reverseNumber > 0) {
+            int digit = reverseNumber % 10;
+            reverseNumber /= 10;
+            switch (digit) {
+                case 0:
+                    System.out.println("Zero");
+                    break;
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Five");
+                    break;
+                case 6:
+                    System.out.println("Six");
+                    break;
+                case 7:
+                    System.out.println("Seven");
+                    break;
+                case 8:
+                    System.out.println("Eight");
+                    break;
+                case 9:
+                    System.out.println("Nine");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (originalDigits != reversedDigits) {
+            for (int i = 0; i < originalDigits - reversedDigits; i++) {
+                System.out.println("Zero");
+            }
+        }
+    }
+
+    public static int reverse(int number) {
+        int reverse = 0;
+        while (number != 0) {
+            reverse = reverse * 10 + number % 10;
+            number /= 10;
+        }
+        return reverse;
+    }
+
+    public static int getDigitCount(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        if (number == 0) {
+            return 1;
+        }
+
+        int count = 0;
+        while (number > 0) {
+            count++;
+            number /= 10;
+        }
+        return count;
+    }
+}
